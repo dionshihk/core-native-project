7aca8a92e099db9bcc0d0db39c0e204398d63708
Dion <dion.shi.hk@gmail.com>
2019-04-08 18:25:04 +0800

    Add missing exports.

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ export {ajax, setRequestInterceptor, setResponseInterceptor} from "./util/networ
 export {call} from "./util/sagaCall";
 export {ErrorBoundary} from "./util/ErrorBoundary";
 
-export {createActionHandlerDecorator, Loading, Interval, Lifecycle, Log, Mutex, Memo} from "./decorator";
+export {createActionHandlerDecorator, createRegularDecorator, Loading, Interval, Lifecycle, Log, Mutex, Memo} from "./decorator";
 export {Exception, APIException, NetworkConnectionException, RuntimeException, ReactLifecycleException} from "./Exception";
 export {showLoading, loadingAction, State} from "./reducer";
 export {register, ErrorListener} from "./module";
